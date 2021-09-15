a2128a94d59db6b41e8dae7f1a54a588b60722e2
Maram Qdah <maram.m.qdah@gmail.com>
2021-09-15 09:20:47 -0400

    add code to eqArray

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(" ✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed ");
+  }
+  
+};
+
+const eqArrays= function(array1,array2) {
+  if(JSON.stringify(array1)==JSON.stringify(array2)){
+   return console.log("true");
+  }else{
+    console.log("not true");
+  }
+}
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"])
+eqArrays(["1", "2", "3"], ["1", "2", 3])
