6bcaa0edd14f87814d6295d1be398a736cf0e157
Maram Qdah <maram.m.qdah@gmail.com>
2021-09-20 18:19:40 -0400

    add mocha and chai to middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,10 @@
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-console.log(middle([1, 2, 3, 4, 5, 6]) );
+//console.log(middle([1, 2, 3, 4, 5, 6]) );
+
+describe("#middle", () => {
+  it("return [3,4] for [1,2,3,4,5,6] ", () =>{
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  })
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,16 @@
 
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
 
 
 // testing the code
-const array=["hi","bye"];
-console.log(tail(array));
+//const array=["hi","bye"];
+//console.log(tail(array));
+
+describe ("#tail", () => {
+  it("return bye for [hi, bye]", () =>{
+    assert.deepEqual(tail(["hi","bye"]),'bye' )
+  })
+})
