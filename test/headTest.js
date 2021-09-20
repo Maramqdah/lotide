a91477699eaa761e04a903e35a16196806676ac2
Maram Qdah <maram.m.qdah@gmail.com>
2021-09-20 13:39:21 -0400

    add test file

---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+
+const assertEqual = require('../head');
+
+console.log(head(["Hello", "Lighthouse", "Labs"]));
+console.log(assertEqual(head([5,6,7]), 5));
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
